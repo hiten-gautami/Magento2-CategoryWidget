b0dabb6e2c9d193b877304644a93ac3de64120e5
gautamihiten <hitengautami.elsner@gmail.com>
2024-05-24 14:50:08 +0530

    Changed vendor name

--- Elsnertech/TextAreaWidget/registration.php
+++ Elsnertech/TextAreaWidget/registration.php
@@ -4,6 +4,6 @@ use \Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'Elsnertech_TextAreaWidget',
+    'Elsnertech_CategoryWidget',
     __DIR__
 );
